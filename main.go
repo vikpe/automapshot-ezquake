419526184aa0b6baa4a41a19a0a3f6198642c47a
Viktor Persson <viktor.persson@arcsin.se>
2022-09-16 11:24:12 +0200

    Add main.

--- main.go
+++ main.go
@@ -1,59 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"time"
-
-	"github.com/joho/godotenv"
-	"github.com/vikpe/go-ezquake"
 )
 
-type MapSettings map[string]string
+func run(args []string) int {
+	app := NewApp()
 
-func main() {
-	// load env
-	err := godotenv.Load()
+	if 1 == len(args) {
+		args = append(args, "--help")
+	}
+
+	err := app.Run(args)
 	if err != nil {
-		fmt.Println("unable to load environment variables", err)
-		return
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		return 1
 	}
 
-	// take screenshots
-	ctrl := ezquake.NewClientController(
-		os.Getenv("EZQUAKE_PROCESS_USERNAME"),
-		os.Getenv("EZQUAKE_BIN_PATH"),
-	)
-
-	if !ctrl.Process.IsStarted() {
-		log.Fatal("ezQuake is not started")
-		return
-	}
-
-	mapSettings := getMapSettingsFromFile("mapsettings.json")
-
-	doCmd := func(cmd string, timeout time.Duration) {
-		ctrl.CommandWithOptions(cmd, ezquake.CommandOptions{Timeout: timeout})
-	}
-
-	for mapName, settings := range mapSettings {
-		fmt.Println(mapName, settings)
-		doCmd(fmt.Sprintf("map %s", mapName), time.Second*4)
-		doCmd(settings, time.Millisecond*250)
-		doCmd("clear", time.Millisecond*250)
-		doCmd("screenshot", time.Second)
-	}
-
-	// todo: move screenshots to /dist
-	// todo: resize screenshots
+	return 0
 }
 
-func getMapSettingsFromFile(filePath string) MapSettings {
-	file, _ := ioutil.ReadFile(filePath)
-	settings := MapSettings{}
-	_ = json.Unmarshal(file, &settings)
-	return settings
+func main() {
+	os.Exit(run(os.Args))
 }
